f403928b32e70b68c6e7cc766ede4efbe387f655
njvision <natjel1982@gmail.com>
2023-04-04 21:23:18 +0300

    update application properties and add message to service layer

--- src/main/java/com/mdbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/mdbookshop/orderservice/order/domain/OrderService.java
@@ -2,32 +2,43 @@ package com.mdbookshop.orderservice.order.domain;
 
 import com.mdbookshop.orderservice.book.Book;
 import com.mdbookshop.orderservice.book.BookClient;
+import com.mdbookshop.orderservice.order.event.OrderAcceptedMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import com.mdbookshop.orderservice.order.event.OrderDispatchedMessage;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
 
-    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+    private final StreamBridge streamBridge;
+
+    public OrderService(OrderRepository orderRepository, BookClient bookClient, StreamBridge streamBridge) {
         this.orderRepository = orderRepository;
         this.bookClient = bookClient;
+        this.streamBridge = streamBridge;
     }
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
+    @Transactional
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient
                 .getBookByIsbn(isbn)
                 .map(acceptedBook -> buildAcceptedOrder(acceptedBook, quantity))
                 .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-                .flatMap(orderRepository::save);
+                .flatMap(orderRepository::save)
+                .doOnNext(this::publishOrderAcceptedEvent);
     }
 
     public static Order buildAcceptedOrder(Book book, int quantity) {
@@ -38,12 +49,22 @@ public class OrderService {
         return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
     }
 
+    private void publishOrderAcceptedEvent(Order order) {
+        if(!order.status().equals(OrderStatus.ACCEPTED)) {
+            return;
+        }
+
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event with id: {}", order.id());
+        var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}", order.id(), result);
+    }
+
     public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
         return flux
                 .flatMap(message -> orderRepository.findById(message.orderId()))
                 .map(this:: buildDispatchedOrder)
                 .flatMap(orderRepository::save);
-
     }
 
     public Order buildDispatchedOrder(Order existingOrder) {
--- src/test/java/com/mdbookshop/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/mdbookshop/orderservice/OrderServiceApplicationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -22,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestChannelBinderConfiguration.class)
 @Testcontainers
 class OrderServiceApplicationTests {
 
